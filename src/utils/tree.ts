2090a658c4fee49cd18d8c1973cb0a945a5cc11a
zhenglingpeng <lingzhengpeng@outlook.com>
2024-07-01 18:14:48 +0800

    fix: test

--- src/utils/tree.ts
+++ src/utils/tree.ts
@@ -13,9 +13,15 @@ export function flattenTrees<T extends { children?: T[] }>(trees: T[] = []): T[]
 }
 
 // 数组转树
-export function TreeToArray<T extends { children?: T[] }>(arr: T[] = []): T[] {
-  return chain((node) => {
-    const children = node.children ? TreeToArray(node.children) : [];
-    return [...children, node];
-  }, arr);
+// export function TreeToArray<T extends { children?: T[] }>(arr: T[] = []): T[] {
+//   return chain((node) => {
+//     const children = node.children ? TreeToArray(node.children) : [];
+//     return [...children, node];
+//   }, arr);
+// }
+
+export function TreeToArray(list: any, root: any): any {
+  return list
+    .filter((item: any) => item.p_c_id === root)
+    .map((item: any) => ({ ...item, children: TreeToArray(list, item.c_id) }));
 }
